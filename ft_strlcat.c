df2ed37192ae6a6cd8e841c10fa11d657b677f41
rbaum <baumraphael@gmail.com>
2014-11-11 15:17:37 +0100

    not sure

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/11 12:07:25 by rbaum             #+#    #+#             */
+/*   Updated: 2014/11/11 12:13:56 by rbaum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char * src, size_t size)
+{
+	size_t ori;
+
+	if (size == 0)
+		return (0);
+	ori = size;
+	while (*dst, size > 0)
+	{
+		dst++;
+		size--;
+	}
+	if (size == 0)
+		return (ori - 1);
+	while (*src, size > 0)
+	{
+		*dst = *src;
+		src++;
+		dst++;
+		size--;
+	}
+	*dst = '\0';
+	return (ori - size);
+}
